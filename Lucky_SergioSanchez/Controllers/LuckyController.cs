b49a916b55aa841627aac0f0567517ff73306ef8
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 19:59:21 -0400

    migracion hecha

---
+++ Lucky_SergioSanchez/Controllers/LuckyController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lucky_SergioSanchez.Models;
+
+namespace Lucky_SergioSanchez.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LuckyController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LuckyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Lucky
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Lucky>>> GetLucky()
+        {
+            return await _context.Lucky.ToListAsync();
+        }
+
+        // GET: api/Lucky/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Lucky>> GetLucky(int id)
+        {
+            var lucky = await _context.Lucky.FindAsync(id);
+
+            if (lucky == null)
+            {
+                return NotFound();
+            }
+
+            return lucky;
+        }
+
+        // PUT: api/Lucky/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLucky(int id, Lucky lucky)
+        {
+            if (id != lucky.SuerteID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(lucky).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LuckyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Lucky
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Lucky>> PostLucky(Lucky lucky)
+        {
+            _context.Lucky.Add(lucky);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLucky", new { id = lucky.SuerteID }, lucky);
+        }
+
+        // DELETE: api/Lucky/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLucky(int id)
+        {
+            var lucky = await _context.Lucky.FindAsync(id);
+            if (lucky == null)
+            {
+                return NotFound();
+            }
+
+            _context.Lucky.Remove(lucky);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LuckyExists(int id)
+        {
+            return _context.Lucky.Any(e => e.SuerteID == id);
+        }
+    }
+}
---
+++ Lucky_SergioSanchez/Migrations/20211013235824_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lucky_SergioSanchez.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lucky",
+                columns: table => new
+                {
+                    SuerteID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Detalle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lucky", x => x.SuerteID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lucky");
+        }
+    }
+}
