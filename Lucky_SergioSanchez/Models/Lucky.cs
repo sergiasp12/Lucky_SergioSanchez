7442ee5ccd5d78b71d2a53d293c5ad87dbcff08b
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 19:38:23 -0400

    modelo lucky acabado

---
+++ Lucky_SergioSanchez/Models/Lucky.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_SergioSanchez.Models
+{
+    public class Lucky
+    {
+        [Key]
+        public int SuerteID { get; set; }
+        [StringLength(100 , MinimumLength =4 ,ErrorMessage ="colocar detalle de 4 a 100")]
+        public string Detalle { get; set; }
+        [Url]
+        public string Imagen { get; set; }
+    }
+}
