33a8f9b1ff25562bd79ddb1841ec9759a3fa25f7
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 20:37:30 -0400

    random terminado

--- Lucky_SergioSanchez/Models/AppDbContext.cs
+++ Lucky_SergioSanchez/Models/AppDbContext.cs
@@ -8,6 +8,8 @@ namespace Lucky_SergioSanchez.Models
 {
     public class AppDbContext:DbContext
     {
+        internal object lucky;
+
         public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
         {
                 
--- Lucky_SergioSanchez/Models/Lucky.cs
+++ Lucky_SergioSanchez/Models/Lucky.cs
@@ -10,9 +10,11 @@ namespace Lucky_SergioSanchez.Models
     {
         [Key]
         public int SuerteID { get; set; }
-        [StringLength(100 , MinimumLength =4 ,ErrorMessage ="colocar detalle de 4 a 100")]
+ 
+        [StringLength(800 , MinimumLength = 1 ,ErrorMessage ="colocar detalle de 4 a 100")]
         public string Detalle { get; set; }
         [Url]
+        [StringLength(800, MinimumLength = 1, ErrorMessage = "la longitud de {0} debe estar entre {2} y {1}")]
         public string Imagen { get; set; }
     }
 }
